67cb81e237c793a215db2a0ad66927f67c50948b
Christophe MOREAU <50195717+ChM40@users.noreply.github.com>
2023-01-31 18:00:11 +0100

    Initial commit

---
+++ TestUno.Wasm/bin/Debug/net7.0/dist/service-worker.js
@@ -0,0 +1,34 @@
+import { config } from "./package_716d403281334319fd200f6deb6033a154aa5c16//uno-config.js";
+
+console.debug("[ServiceWorker] Initializing");
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        caches.open('package_716d403281334319fd200f6deb6033a154aa5c16').then(function (cache) {
+            console.debug('[ServiceWorker] Caching app binaries and content');
+            return cache.addAll(config.offline_files);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed
---
+++ TestUno.Wasm/bin/release/net7.0/dist/service-worker.js
@@ -0,0 +1,34 @@
+import { config } from "./package_3d5757c0989735e593e40b382ff1bdf36ad70559//uno-config.js";
+
+console.debug("[ServiceWorker] Initializing");
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        caches.open('package_3d5757c0989735e593e40b382ff1bdf36ad70559').then(function (cache) {
+            console.debug('[ServiceWorker] Caching app binaries and content');
+            return cache.addAll(config.offline_files);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed
